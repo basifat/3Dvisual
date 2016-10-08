87bea6227d07597451c40f6ed51d0c07bf169109
basifat <babatunde.asifat@gmail.com>
2016-10-08 16:51:02 +0300

    updated read input file

--- 3Dvisual.py
+++ 3Dvisual.py
@@ -2,13 +2,15 @@
     
     Usage:
         3Dvisual.py -h
-		3Dvisual.py [(--draw-axes) (--draw-reference) (--draw-ref-labels) (--sizeX=<number>) (--sizeY=<number>) (--sizeZ=<number>) (--draw-X-values) (--draw-Y-values) (--draw-Z-values)]
-	
-        
+		3Dvisual.py (-f <filename>) [(--draw-axes) (--draw-reference) (--draw-ref-labels) (--sizeX=<number>) (--sizeY=<number>) (--sizeZ=<number>) (--draw-X-values) (--draw-Y-values) (--draw-Z-values)]
 
-    Options:
-		-h,--help         : show this help message
-		--draw-axes       : example flag #1
+		
+	  
+
+	Options:
+		-h,--help : This is the help message. Provide the file name as the first argument, followed by the other command to be run by the user.
+		-f : specify the filename or the file path. i.e -f filename.csv 
+		--draw-axes       : draw axes of plot
 		--draw-reference  : draw reference of plot
 		--draw-ref-labels : draw reference labels
 		--draw-X-values   : draw X values on reference plot
@@ -19,15 +21,15 @@
 		--sizeZ=<number>  : Scale Z by number
 	
 """
-import matplotlib.delaunay as triang  #import delauny triangulation from matplotlib
+import matplotlib.delaunay as triang  #import Delaunay triangulation from Matplotlib
 import matplotlib.text as textlabel
 import matplotlib.colors as cl
 import pylab
 import random
 import numpy
-import numpy  as np  #Import mathematical library numpy
+import numpy  as np  #import numpy library 
 import matplotlib.pyplot as plt
-from docopt import docopt  #Import library to process CLI functions
+from docopt import docopt  #import docopt; the library to process CLI functions
 
 class Draw(object):
 	
@@ -35,14 +37,14 @@ class Draw(object):
 		self.xList = [] #This list holds the values corresponding to X from the CSV
 		self.yList = [] #This list holds values corresponding to Y from the CSV
 		self.zList = [] #This list holds values corresponding to Z from the CSV
-		self.colourList = [] #This list holds values corresponding to colour from the CSV
-		input_file = "test3.csv"
-		with open(input_file, 'r') as input:
+		self.colourList = [] #This list holds values corresponding to hex values of colour from the CSV file
+		input_file = args['<filename>'] # Get the file name from the command line
+		with open(input_file, 'r') as input: #open the CSV file and read through it
 			lines = input.readlines()[1:]
 			for line in lines:
 				line = line.strip()
 				if line:
-					line = line.split("\t")
+					line = line.split("\t") 
 					x = line[0]
 					y = line[1]
 					z = line[2]
@@ -51,20 +53,19 @@ class Draw(object):
 					self.yList.append(y)
 					self.zList.append(z)
 					self.colourList.append("#"+colourhex)
-		len_xList = len(self.xList)
-		len_yList = len(self.yList)
-		len_zList = len(self.zList)
-		len_colourList = len(self.colourList)
-		x_min = int(min(self.xList)) #Min x value from csv data
-		y_min = int(min(self.yList)) #Min y value from csv data
-		z_min = int(min(self.zList)) #Min z value from csv data
-		x_max = int(max(self.xList)) #Max x value from csv data
-		y_max = int(max(self.yList)) #Max y value from csv data
-		z_max = int(max(self.zList)) #Max z value from csv data
-		#X,Y,Z min tuple
-		self.min_xyz_values = (x_min,y_min,z_min) #Tuple holding the x,y, and z minimum values respectively
-		self.max_xyz_values = (x_max,y_max,z_max) #Tuple holding the x,y and z maximum values respectively
-		self.xyzcolour_len = (len_xList,len_yList,len_zList,len_colourList) #Tuple holding the len of x,y,z and colour list respectively
+		self.len_xList = len(self.xList)
+		self.len_yList = len(self.yList)
+		self.len_zList = len(self.zList)
+		self.len_colourList = len(self.colourList)
+		x_min = int(min(self.xList)) #Min x value from CSV data
+		y_min = int(min(self.yList)) #Min y value from CSV  data
+		z_min = int(min(self.zList)) #Min z value from CSV  data
+		x_max = int(max(self.xList)) #Max x value from CSV  data
+		y_max = int(max(self.yList)) #Max y value from CSV  data
+		z_max = int(max(self.zList)) #Max z value from CSV  data
+		self.min_xyz_values = (x_min,y_min,z_min) #Tuple holding the minimum values of x, y and z respectively
+		self.max_xyz_values = (x_max,y_max,z_max) #Tuple holding the maximum values of x, y and z respectively
+		self.xyzcolour_len = (self.len_xList,self.len_yList,self.len_zList,self.len_colourList) #Tuple holding the size of the list of x,y,z and colour respectively
 		colour = cl.hex2color(self.colourList[0])
 		self.R = colour[0]
 		self.G = colour[1]
@@ -79,7 +80,6 @@ class Draw(object):
 		# t[0], t[1], t[2] are the points indexes of the triangle
 		#Now we want to extract a complete triangle from each index
 			t_i = [t[0], t[1], t[2], t[0]]
-			#t_i = [t[0], t[1], t[2]]
 			x = [self.xList[i] for i in t_i]
 			y = [self.yList[i] for i in t_i]
 			z = [self.zList[i] for i in t_i]
@@ -92,17 +92,16 @@ class Draw(object):
 			x = [int(i) for i in x]
 			y = [int(i) for i in y]
 			z = [int(i) for i in z]
-			#For performance sake, we reuse the xList, yList, zList and colourList holding data from the
-			#csv (insead of a new list lst_x e.t.c)
+			#For performance sake, we reuse the xList, yList, zList and colourList(holding original CSV data) instead of creating and using a new lists 
 			self.xList.append(x)
 			self.yList.append(y)
 			self.zList.append(z)
 			self.colourList.append(colourhex)
-		#Update self.xList,self.yList,self.zList and self.colourList to the new data needed for triangulation
-		self.xList[:100] = [] #Remove all values corresponding to X that was appended from csv
-		self.yList[:100] = [] #Remove all values corresponding to Y that was appended from csv
-		self.zList[:100] = [] #Remove all values corresponding to Z that was appended from csv
-		self.colourList[:100] = [] #Remove all values corresponding to colour that was appended from csv
+		#Update the content of xList,yList,zList and colourList to the new data needed for triangulation
+		self.xList[:self.len_xList] = [] #Remove all X values original appended from the CSV file 
+		self.yList[:self.len_yList] = [] #Remove all Y values original appended from the CSV file
+		self.zList[:self.len_zList] = [] #Remove all Z values original appended from the CSV file
+		self.colourList[:self.len_colourList] = [] #Remove all hex values original appended from the CSV
 
 	def draw_triangulation(self):
 		col = 0 #Start at column 0
@@ -114,13 +113,13 @@ class Draw(object):
 		#G = colour[j]
 		#B = colour[k]
 		print "#gl_cmds="  #Before a new plot, clear the previous plot
-		#Remember that we updated self.xList,self.yList,self.zList and self.colourList from the csv data values now to the data needed for triangulation or plotting
+		#Remember on lines #101 through #104 we updated self.xList,self.yList,self.zList and self.colourList from the CSV data values to the data needed for triangulation/plotting
 		
-		while (col<len(self.xList)): #Iterate through the entire len of  xList
-			#Access lists column after column
-			#Swap y and Z values for our plot
-			xcol = self.xList[col] #Get all values corresponding to x from updated xList
-			zcol = self.yList[col] #Get all values corresponding to y from updated yList
+		while (col<len(self.xList)): #Starting at the first index, iterate through the entire length of  xList
+			#Access lists values column by column
+			#Swap y and Z values for the purpose of our plot
+			xcol = self.xList[col] #Get all values corresponding to x from xList
+			zcol = self.yList[col] #Get all values corresponding to y from yList
 			ycol = self.zList[col] #Get all values corresponding to z from zList
 			#Get out each individual items in the columns
 			x1 = xcol[i] 
@@ -137,22 +136,23 @@ class Draw(object):
 			R = self.R
 			B = self.B
 			G = self.G
-			#Increament col by 1 after every iteration so that we can go on to the next column
+			#Increament col to further to the next column on next iteration
 			col = col + 1
 			i = 0
-			#Plot and print traingulation
+			#Render our plot using the triangulation points 
+			#Aurora-VR Mod understands understands this print command to generate our plot
 			print "line", x1,y1,z1,x2,y2,z2,thickness,R,G,B,gamma
 			print "line", x2,y2,z2,x3,y3,z3,thickness,R,G,B,gamma
 			print "line", x3,y3,z3,x1,y1,z1,thickness,R,G,B,gamma
-			print "text", 0,y1,0,"ffffff", y1 #Draw x values 
-			print "text", 0,y2,0,"ffffff", y2 #Draw x values 
-			print "text", 0,y3,0,"ffffff", y3 #Draw x values 
-			print "text", x1,0,0,"ffffff", x1 #Draw y values 
-			print "text", x2,0,0,"ffffff", x2 #Draw y values 
-			print "text", x3,0,0,"ffffff", x3 #Draw y values 
+			print "text", 0,y1,0,"ffffff", y1 #Draw y values 
+			print "text", 0,y2,0,"ffffff", y2 #Draw y values 
+			print "text", 0,y3,0,"ffffff", y3 #Draw y values 
+			print "text", x1,0,0,"ffffff", x1 #Draw x values 
+			print "text", x2,0,0,"ffffff", x2 #Draw x values 
+			print "text", x3,0,0,"ffffff", x3 #Draw x values 
 			print "text", 0,0,z1,"ffffff", z1 #Draw z values 
 			print "text", 0,0,z2,"ffffff", z2 #Draw Z values 
-			print "text", 0,0,z3,"ffffff", z3 #Draw Z values
+			print "text", 0,0,z3,"ffffff", z3 #Draw z values
 
 	
 def main(docopt_args):
@@ -163,7 +163,7 @@ def main(docopt_args):
 	z_min = obj.min_xyz_values[1] #y_min value is now swapped for z_min value
 	y_min = obj.min_xyz_values[2] #z_min value is now swapped for y_min value
 	x_max = obj.max_xyz_values[0]
-	#Same swapping for both z_max and y_max
+	#Swap also the values of z_max and y_max
 	z_max = obj.max_xyz_values[1] 
 	y_max = obj.max_xyz_values[2]
 	R = obj.R
@@ -173,30 +173,33 @@ def main(docopt_args):
 	range_x= x_max - x_min #Range of x values
 	range_y= y_max - y_min #Range of y values
 	range_z= z_max - z_min #Range of y values
-	#Calculate the triangulation data
+	#Calculate our triangulation data
 	obj.calc_triangulation()
-	
-	#Check for scaling parameters and apply accordingly
-	if docopt_args["--sizeX"]: #If the is a value, scale X for this value
+	#in order to scale our plot, we need to check for the following parameters
+	#and apply accordingly
+	#Scale X plot by the value provided at CLI
+	if docopt_args["--sizeX"]: 
 		number = docopt_args.get("--sizeX")
 		scale_x = (int(number)/range_x)
 		obj.xList = np.multiply(obj.xList,int(scale_x)) #Scale the values in xList by scale_x value
 		x_min = np.amin(obj.xList) #Min x value after scaling X
 		x_max = np.amax(obj.xList) #Max x value after scaling X
-	if docopt_args["--sizeY"]: #if there is a value, scale Y for this value
+	#Scale Y plot by the value provided at CLI
+	if docopt_args["--sizeY"]: 
 		number = docopt_args.get("--sizeY")
 		scale_y = (int(number)/range_y)
 		obj.zList = np.multiply(obj.zList,int(scale_y)) #Scale elements contained in zList, these will will use as y values
 		y_min = np.amin(obj.zList) #Min y value after scaling
 		y_max = np.amax(obj.zList)  #Max y value after scaling
 		print "damn y min", y_min
-	if docopt_args["--sizeZ"]: #if there is a value, scale Z for this value
+	#Scale Z plot by the value provided at CLI
+	if docopt_args["--sizeZ"]: 
 		number = docopt_args.get("--sizeZ")
 		scale_z = (int(number)/range_z)
 		obj.yList = np.multiply(obj.yList,int(scale_z))
 		z_min = np.amin(obj.yList) #Min z value after scaling
 		z_max = np.amax(obj.yList)  #Max z value after scaling
-	#After all scaling is done, print triangulation plot
+	#After all scaling is done, draw triangulation plot
 	obj.draw_triangulation()
 	
 	#Draw triangulation axes
--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # 3Dvisual
 main program
 
-How to run:
-
-Step1:
+Set up:
 
 1. If python is not installed, install latest Python(x,y) from this location:
 https://code.google.com/p/pythonxy/wiki/Downloads
@@ -13,3 +11,5 @@ https://code.google.com/p/pythonxy/wiki/Downloads
 Run pip install -r requirements.txt
 
 If Numpy or Matplotlib fails to install, you may need to create a virtual environment and install the Python(x,y)
+
+
